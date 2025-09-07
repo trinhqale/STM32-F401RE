7e31f1e8407456d94b8920c4f49b23e37d587712
Quoc-Anh Le <qale@Quoc-Anhs-MacBook-Air.local>
2025-09-06 23:02:41 -0700

    Update dockerfile to be compatible with MacOS; Update project.mk to baseline config;Added dockerignore

--- STM32-F401RE/project.mk
+++ STM32-F401RE/project.mk
@@ -1,15 +1,16 @@
-# project.mk — H743
-override TARGET    = MyApp-H743
-override C_DEFS    = -DUSE_HAL_DRIVER -DSTM32H743xx
-override CPU       = -mcpu=cortex-m7
-override FPU       = -mfv5-d16
+# project.mk — F401RE
+override TARGET    = STM32-F401RE
+override C_DEFS    = -DUSE_HAL_DRIVER -DSTM32F401xE
+override CPU       = -mcpu=cortex-m4
+override FPU       = -mfpu=fpv4-sp-d16
 override FLOAT-ABI = -mfloat-abi=hard
-override LDSCRIPT  = toolchain/STM32H743_FLASH.ld
+override LDSCRIPT  = STM32F401XX_FLASH.ld
+# HAL/CMSIS include dirs for F4:
 override C_INCLUDES = \
   -ICore/Inc \
-  -IDrivers/STM32H7xx_HAL_Driver/Inc \
-  -IDrivers/STM32H7xx_HAL_Driver/Inc/Legacy \
-  -IDrivers/CMSIS/Device/ST/STM32H7xx/Include \
+  -IDrivers/STM32F4xx_HAL_Driver/Inc \
+  -IDrivers/STM32F4xx_HAL_Driver/Inc/Legacy \
+  -IDrivers/CMSIS/Device/ST/STM32F4xx/Include \
   -IDrivers/CMSIS/Include
-# Match your startup file:
-override ASM_SOURCES = startup_stm32h743xx.s
+# If your startup filename differs:
+# override ASM_SOURCES = startup_stm32f401xe.s
